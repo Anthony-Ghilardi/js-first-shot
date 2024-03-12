7dfc1e36bda595bc60b57f79f06f4780efa41157
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-03-11 21:28:08 -0700

    wip

---
+++ index.js
@@ -0,0 +1,15 @@
+let jerseyColors = ['Red', 'Black', 'Orange', 'Blue', 'Purple', 'Yellow', 'Brown']
+
+console.log (`The team's jersey color will be ${jerseyColors[3]}, ${jerseyColors[2]}, ${jerseyColors[4]}.`)
+
+console.log(jerseyColors)
+
+jerseyColors.push('Maroon')
+console.log(jerseyColors)
+
+
+// console.log("The team's jersey color will be " + jerseyColors[3] + '.')
+
+// alternatively you could type console.log('The team/'s jersey color will b ' + jerseyColor[3] + '.') to continue using all single quotes ' ' 
+
+// ADVANCED ALTERNATIVE USING THE GRAVE KEY AKA SHIFT + TILDA console.log (`The team's jersey color will be ${jerseyColors[3]}.`) 
