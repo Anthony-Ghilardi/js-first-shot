d22352eb2769cc0d734673b02d08f6cb448dd6c0
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-03-13 19:12:39 -0700

    WIP

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* Arrays, declaring variables, console.log 
+// Day one start
+
 let jerseyColors = ['Red', 'Black', 'Orange', 'Blue', 'Purple', 'Yellow', 'Brown']
 
 console.log (`The team's jersey color will be ${jerseyColors[3]}, ${jerseyColors[2]}, ${jerseyColors[4]}.`)
@@ -7,9 +10,30 @@ console.log(jerseyColors)
 jerseyColors.push('Maroon')
 console.log(jerseyColors)
 
-
 // console.log("The team's jersey color will be " + jerseyColors[3] + '.')
 
 // alternatively you could type console.log('The team/'s jersey color will b ' + jerseyColor[3] + '.') to continue using all single quotes ' ' 
 
 // ADVANCED ALTERNATIVE USING THE GRAVE KEY AKA SHIFT + TILDA console.log (`The team's jersey color will be ${jerseyColors[3]}.`) 
+
+// Day one ends 
+*/
+
+// Day two starts
+
+// console.log (0 == false)
+// console.log ('' == false)
+
+// let a = 0
+// let b = 0
+
+// console.log (a === b)
+
+let a = '0'
+let b = '0'
+
+console.log (a === b)
+
+if (a==b){
+    console.log('A is the same as b. The value of a is ${a} and the value of b is {b}')
+}
